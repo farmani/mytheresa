d27626cf60edbeae2b0d941391f61a2803648aab
Ramin Farmani <ramin.farmani@gmail.com>
2025-12-25 19:41:17 +0700

    Add JSON response handling to OKResponse and ErrorResponse functions

--- app/api/response.go
+++ app/api/response.go
@@ -1,11 +1,24 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func OKResponse(w http.ResponseWriter, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
 
 func ErrorResponse(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	err := json.NewEncoder(w).Encode(map[string]string{"error": message})
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
